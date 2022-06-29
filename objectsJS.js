7cd30e13202308d98636d29a44728d5f8e9e643d
Kevin Bell <bellKevin@pm.me>
2022-06-29 01:28:52 +0000

    Create objectsJS.js

---
+++ objectsJS.js
@@ -0,0 +1,14 @@
+function animal(obj){
+  var animal ={name: "dog", legs: 4, color: "white"}
+   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+function animal(obj){
+  var animal = {name:"cock",legs:2,color:"red"}
+   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+function animal(obj){
+  var animal = {name:"rabbit",legs:4,color:"gray"}
+   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
